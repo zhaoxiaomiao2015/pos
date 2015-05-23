458c1bab8f8a08ec0e03176976c28d268cecc5eb
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-23 21:30:23 +0800

    first commit

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -6,12 +6,13 @@ function printInventory(inputs) {
     var footte = '**********************';
 
     for(var i=0; i< inputs.length; i++){
-        //得到数组i个子集和inputs[i]
-        //取得子集和的每个元素 如：取到名称--inputs[i].name;
-    //    head = head + '名称：'+inputs[i].name+' ,'+'数量：'+inputs[i].count+'瓶，单价：'+inputs[i].price.toFixed(2)+'(元)，小计：'+(inputs[i].price*inputs[i].count).toFixed(2)+'(元)\n';
-        head = head + '名称：'+inputs[i].name+'，'+'数量：'+inputs[i].count+inputs[i].unit+'，单价：'+inputs[i].price.toFixed(2)+'(元)，小计：'+(inputs[i].price*inputs[i].count).toFixed(2)+'(元)\n';
+        head = head + '名称：'+inputs[i].name+'，'+
+        '数量：'+inputs[i].count+inputs[i].unit+
+        '，单价：'+inputs[i].price.toFixed(2)+
+        '(元)，小计：'+(inputs[i].price*inputs[i].count).toFixed(2)+'(元)\n';
         sum = sum + inputs[i].price*inputs[i].count;
     }
+    
     var result = head + conter + '总计：' + sum.toFixed(2) + '(元)\n' +   footte;
     console.log(result);
 
