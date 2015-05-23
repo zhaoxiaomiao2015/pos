623a1d085f33df3a6b67a856b34530d636f054e6
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-23 21:27:23 +0800

    last commit pos_V1

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,7 +1,25 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-
     var newInputs = [];
+    newInputs = repeat(inputs);
+    cupBarcode(newInputs);
+    var allCommoditiesArray = loadAllItems();
+    var fullInformationArray = [];
+    fullInformationArray = getAllInformation(newInputs,allCommoditiesArray);
+    var preferentialGoodsBarcodeArray = loadPromotions();
+    preferentialGoodsBarcode = preferentialGoodsBarcodeArray[0].barcodes;
+    var allGoodsOutput = "";
+    var preferentialGoodsResult = "";
+    var sum = 0;
+    var save = 0;
+    var subtotal = 0;
+    preferentialJudge(fullInformationArray,preferentialGoodsBarcode);
+    console.log('***<没钱赚商店>购物清单***\n' +
+            allGoodsOutput +'----------------------\n' +
+            '挥泪赠送商品：\n' + preferentialGoodsResult +
+            '----------------------\n' +
+            shouTotal() + '**********************');
+
     function repeat(inputs){
         for (var i = 0; i < inputs.length; i++) {
             var exist = false;
@@ -18,22 +36,25 @@ function printInventory(inputs) {
                 newInputs.push(newItems);
             }
         }
+        return newInputs;
     }
 
-    repeat(inputs);
-
-    for (i = 0; i < newInputs.length; i ++) {
-        var exist = newInputs[i].barcode.indexOf('-');
-        if (exist != -1) {
-            var temp = newInputs[i].barcode.split('-');
-            newInputs[i].count = temp[1];
-            newInputs[i].barcode = temp[0];
+    function cupBarcode(newInputs ){
+        for (i = 0; i < newInputs.length; i ++) {
+            var exist = newInputs[i].barcode.indexOf('-');
+            if (exist != -1) {
+                newInputs[i].count = 0;
+                // newInputs[i].barcode = newInputs[i].barcode.substring(0,exist);
+                // newInputs[i].count = newInputs[i].barcode.substring(11);
+                // console.log(newInputs[i].barcode.split('-'));
+                var temp = newInputs[i].barcode.split('-')
+                newInputs[i].count = temp[1];
+                newInputs[i].barcode = temp[0];
+            }
         }
     }
 
-    var allCommoditiesArray = loadAllItems();
-    var fullInformationArray = [];
-
+    function getAllInformation(newInputs,allCommoditiesArray) {
         for (i = 0; i < newInputs.length; i++) {
         for ( j=0; j<allCommoditiesArray.length; j++) {
             if (newInputs[i].barcode === allCommoditiesArray[j].barcode) {
@@ -47,31 +68,33 @@ function printInventory(inputs) {
                 }
             }
         }
+        return fullInformationArray;
+    }
 
-    var preferentialGoodsBarcodeArray = loadPromotions();
-    preferentialGoodsBarcode = preferentialGoodsBarcodeArray[0].barcodes;
-    var preferentialBeforeSubtotal = 0;
-    var preferentianlAfterSubtotal = 0;
-    var allGoodsOutput = "";
-    var preferentialGoodsResult = "";
-    var sum = 0;
-    var save = 0;
-    for (i = 0; i < fullInformationArray.length; i++) {
-        subtotal = fullInformationArray[i].price *fullInformationArray[i].count;
-        for (j = 0; j< preferentialGoodsBarcode.length; j++) {
-            if (fullInformationArray[i].barcode === preferentialGoodsBarcode[j]) {
-                //计算商品价格（买二送一）
-                 subtotal = fullInformationArray[i].price *(fullInformationArray[i].count - Math.floor(fullInformationArray[i].count/3));
-                 break;
+    function preferentialJudge( fullInformationArray,preferentialGoodsBarcode){
+        for (i = 0; i < fullInformationArray.length; i++) {
+            subtotal = fullInformationArray[i].price *fullInformationArray[i].count;
+            for (j = 0; j< preferentialGoodsBarcode.length; j++) {
+                if (fullInformationArray[i].barcode === preferentialGoodsBarcode[j]) {
+                    //计算商品价格（买二送一）
+                     subtotal = fullInformationArray[i].price *(fullInformationArray[i].count - Math.floor(fullInformationArray[i].count/3));
+                     break;
+                }
             }
+            shouAllInformation(i);
+            showPreferentialInformation(i);
+            sum += subtotal;
         }
+    }
 
+    function shouAllInformation(i){
         allGoodsOutput += '名称：'+fullInformationArray[i].name+
                         '，数量：'+fullInformationArray[i].count+fullInformationArray[i].unit+
                         '，单价：'+fullInformationArray[i].price.toFixed(2)+'(元)，'+
                         '小计：'+subtotal.toFixed(2)+'(元)\n';
-        sum += subtotal;
+    }
 
+    function showPreferentialInformation(i){
         if (fullInformationArray[i].barcode === preferentialGoodsBarcode[j]) {
             var giveNumber =  Math.floor(fullInformationArray[i].count/3);
             preferentialGoodsResult += '名称：'+fullInformationArray[i].name+
@@ -80,14 +103,9 @@ function printInventory(inputs) {
         }
     }
 
-    var res = '总计：'+sum.toFixed(2)+'(元)\n' +
-    '节省：'+save.toFixed(2)+'(元)\n';
-
-    console.log('***<没钱赚商店>购物清单***\n' +
-     allGoodsOutput +'----------------------\n' +
-     '挥泪赠送商品：\n' +
-     preferentialGoodsResult +
-     '----------------------\n' +
-     res +
-     '**********************');
+    function shouTotal(){
+        var total = '总计：'+sum.toFixed(2)+'(元)\n' +
+            '节省：'+save.toFixed(2)+'(元)\n';
+        return total;
+        }
     }
